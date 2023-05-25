c046277068c6f92c12ea582eb6035c4acd7c417a
tgj505 <johnson.tom.g@gmail.com>
2023-05-25 14:53:23 -0400

    reconciling with main branch

--- greysend_project/accounts/urls.py
+++ greysend_project/accounts/urls.py
@@ -5,9 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('members', views.list_users, name='list_users'),
     path('signup',views.signup, name='signup'),
-    path("<str:user>/", views.profile_view, name='profile_view')
+    path("<str:user>/", views.profile_view, name='profile_view'),
+    path('log_out', views.log_out, name='logout'),
+    path('log_in', views.log_in, name="login"),
 ]
-
-
-# re_path(r"^articles/(?P<year>[0-9]{4})/$", views.year_archive),
-# re_path(r"^bio/(?P<username>\w+)/$", views.bio, name="bio"),
--- greysend_project/accounts/views.py
+++ greysend_project/accounts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import AccountUser
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -26,9 +26,27 @@ def signup(request):
             user.save()
             login(request, user)
         return render(request, 'signup.html', {"form":UserCreationForm})  
-# Compare this snippet from greysend_project/accounts/models.py:
-
 
 def profile_view(request, user):
     p_user = User.objects.get(username=user)
     return render(request, 'profile_view.html', {"user": p_user})
+
+def log_in(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {'form': AuthenticationForm})
+    elif request.method=='POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            #to implement "profile.html"
+            return redirect('list_users')  
+        else:
+            #todo: implement error page
+            return redirect("error_no_user.html")
+        
+def log_out(request):
+    #get current user
+    logout(request)
+    return redirect('index')
